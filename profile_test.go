417f79433690982146275d0e935e0fd467192bd7
Dave Cheney <dave@cheney.net>
2014-10-22 12:51:30 +1100

    Revert "Added more examples"
    This reverts commit f63e944a26e392dcac6c1006ab2e01ca012d6a86.

--- profile_test.go
+++ profile_test.go
@@ -1,8 +1,6 @@
 package profile_test
 
 import (
-	"os"
-
 	"github.com/davecheney/profile"
 )
 
@@ -12,23 +10,11 @@ func ExampleStart() {
 	defer profile.Start().Stop()
 }
 
-func ExampleCPUProfile() {
-	// CPU profiling is the default profiling mode, but you can specify it
-	// explicitly for completeness.
-	defer profile.Start(profile.MemProfile).Stop()
-}
-
 func ExampleMemProfile() {
-	// use memory profiling, rather than the default cpu profiling.
 	defer profile.Start(profile.MemProfile).Stop()
-}
-
-func ExampleProfilePath() {
-	// set the location that the profile will be written to
-	defer profile.Start(profile.ProfilePath(os.Getenv("HOME")))
 }
 
 func ExampleNoShutdownHook() {
-	// disable the automatic shutdown hook.
-	defer profile.Start(profile.NoShutdownHook).Stop()
+	// start a CPU profileri with a custom path.
+	defer profile.Start(profile.CPUProfile, profile.ProfilePath("/home/dfc"), profile.NoShutdownHook).Stop()
 }
